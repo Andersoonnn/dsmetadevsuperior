bd8686546690b13044f032cddc0fca84170e8cd8
Anderson-Pereira-Dev <pereira818@hotmail.com>
2022-09-06 00:19:35 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
   return (
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
