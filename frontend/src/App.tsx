d9896eb84ea2e734f7c4f95636968c4a16ee3030
Anderson-Pereira-Dev <pereira818@hotmail.com>
2022-09-10 21:51:08 -0300

    terceiro comitt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import Header from "./componentes/Header";
 import SalesCard from "./componentes/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+    <ToastContainer/>
     <Header/>
     <main>
       <section id="sales">
